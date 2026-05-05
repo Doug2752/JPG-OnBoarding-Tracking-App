c5cc90a7ad98799cdb7fa2c195fb6bbe5b7f774e
Doug2752 <sycowboy100@gmail.com>
2026-05-05 08:04:09 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import OBApp from './app/OBApp.js';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(React.createElement(OBApp));
